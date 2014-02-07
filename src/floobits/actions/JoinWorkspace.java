3374e87faa6bc5b8b9785562e56c2a667b9619e2
Matt Kaniaris <mkaniaris@gmail.com>
2014-02-07 14:18:40 -0800

    prepop join workspace with dotfloo ur.

--- src/floobits/actions/JoinWorkspace.java
+++ src/floobits/actions/JoinWorkspace.java
@@ -5,6 +5,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import floobits.FlooContext;
 import floobits.FloobitsApplication;
 import floobits.FloobitsPlugin;
+import floobits.common.DotFloo;
+import floobits.common.FlooUrl;
 import floobits.utilities.Flog;
 
 import javax.swing.*;
@@ -13,12 +15,18 @@ import javax.swing.*;
 public class JoinWorkspace extends AnAction {
 
     public void actionPerformed(AnActionEvent e) {
-        String inputValue = JOptionPane.showInputDialog("Workspace URL", "https://floobits.com/");
+        String url = "https://floobits.com/";
+//        Flog.info(inputValue);
+        FlooContext context = FloobitsPlugin.getInstance(e.getProject()).context;
+        FlooUrl floourl = DotFloo.read(context.project.getBasePath());
+        if (floourl != null) {
+            url = floourl.toString();
+        }
+        String inputValue = JOptionPane.showInputDialog("Workspace URL", url);
         if (inputValue == null) {
             return;
         }
-        Flog.info(inputValue);
-        FlooContext context = FloobitsPlugin.getInstance(e.getProject()).context;
+
         FloobitsApplication.self.joinWorkspace(context, inputValue);
     }
 }
