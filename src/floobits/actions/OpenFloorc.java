a9a04ce4ebd860721342033624a6722e34ec1529
Matt Kaniaris <mkaniaris@gmail.com>
2014-02-18 18:56:29 -0800

    add open floorc command.

---
+++ src/floobits/actions/OpenFloorc.java
@@ -0,0 +1,51 @@
+package floobits.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import floobits.FloobitsPlugin;
+import floobits.common.Settings;
+import floobits.common.Utils;
+import floobits.utilities.Flog;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by kans on 2/18/14.
+ */
+public class OpenFloorc extends AnAction {
+    public void actionPerformed(AnActionEvent actionEvent) {
+        Project project = actionEvent.getProject();
+        FloobitsPlugin floobitsPlugin = FloobitsPlugin.getInstance(project);
+        File file = new File(Settings.floorcPath);
+        if (!file.exists()) {
+            boolean created;
+            try {
+                created = file.createNewFile();
+            } catch (IOException e) {
+                floobitsPlugin.context.error_message("Can not create ~/.floorc");
+                Flog.warn(e);
+                return;
+            }
+            if (!created) {
+                floobitsPlugin.context.error_message("Can not create ~/.floorc");
+                return;
+            }
+            Settings settings = new Settings(floobitsPlugin.context);
+            settings.set("#I am emtpy:", "https://floobits.com/help/floorc/");
+            settings.write();
+        }
+        VirtualFile virtualFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(Settings.floorcPath);
+        if (virtualFile == null) {
+            floobitsPlugin.context.error_message("no virtual file");
+            return;
+        }
+        OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, virtualFile);
+        FileEditorManager.getInstance(project).openTextEditor(openFileDescriptor, true);
+    }
+}
