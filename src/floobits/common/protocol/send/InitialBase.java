19b3255c9873c443952d29cdee3ffb9bee17fde8
Bjorn Tipling <bjorn@ambientchill.com>
2014-03-17 12:33:42 -0700

    Use constants for version. Updates version.

--- src/floobits/common/protocol/send/InitialBase.java
+++ src/floobits/common/protocol/send/InitialBase.java
@@ -1,10 +1,11 @@
 package floobits.common.protocol.send;
 
 import com.intellij.openapi.application.ApplicationInfo;
+import floobits.common.Constants;
 import floobits.common.protocol.Base;
 
 public abstract class InitialBase implements Base {
     public String platform = System.getProperty("os.name");
-    public String version = "0.10";
+    public String version = Constants.version;
     String client = ApplicationInfo.getInstance().getVersionName();
 }
