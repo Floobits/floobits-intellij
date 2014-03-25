d4e5af76db32f070139b5e78c223ab5f170d810e
Matt Kaniaris <mkaniaris@gmail.com>
2014-03-24 17:46:19 -0700

    warn users at start to pick the correct dir

--- src/floobits/utilities/SelectFolder.java
+++ src/floobits/utilities/SelectFolder.java
@@ -9,6 +9,8 @@ public class SelectFolder {
 
     public static void build(final RunLater<String> runLater) {
         FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false);
+        descriptor.setTitle("Select Folder For Workspace");
+        descriptor.setDescription("NOTE: Floobits will NOT make a new, root directory inside the folder you choose. If you have cloned the project already, select that folder.");
         VirtualFile[] vFiles = FileChooser.chooseFiles(descriptor, null, null);
         if (vFiles.length == 0) {
             return;
