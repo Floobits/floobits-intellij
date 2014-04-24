73185d705c701dbed4b19df57b07552293c6379d
Bjorn Tipling <bjorn@ambientchill.com>
2014-04-23 17:42:24 -0700

    Create a permissions form. #147

---
+++ src/floobits/dialogs/SetPermissionsForm.java
@@ -0,0 +1,11 @@
+package floobits.dialogs;
+
+import javax.swing.*;
+
+public class SetPermissionsForm {
+    private JCheckBox viewCheckbox;
+    private JCheckBox requestPermCheckbox;
+    private JCheckBox editCheckbox;
+    private JCheckBox adminCheckbox;
+    private JLabel permissionsLabel;
+}
