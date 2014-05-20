d69574def5f8afa2040b558cb32eafea0560a75f
Matt Kaniaris <mkaniaris@gmail.com>
2014-05-20 14:18:19 -0700

    better UI on not adding files

--- src/floobits/actions/AddToWorkspace.java
+++ src/floobits/actions/AddToWorkspace.java
@@ -25,12 +25,19 @@ public class AddToWorkspace extends IsJoinedAction {
                 continue;
             }
             if (!Utils.isSharable(virtualFile)) {
+                context.statusMessage(String.format("Skipped adding %s because it is a special file.", virtualFile.getPath()), false);
                 continue;
             }
             if (!context.isShared(virtualFile.getPath())) {
+                context.statusMessage(String.format("Skipped adding %s because it is not in %s.", virtualFile.getPath(), context.colabDir), false);
                 continue;
             }
-            ArrayList<VirtualFile> allValidNestedFiles = Utils.getAllValidNestedFiles(context, virtualFile);
+            if (context.isIgnored(virtualFile)) {
+                context.statusMessage(String.format("Skipped adding %s because it is ignored.", virtualFile.getPath()), false);
+                continue;
+            }
+
+        ArrayList<VirtualFile> allValidNestedFiles = Utils.getAllValidNestedFiles(context, virtualFile);
             filesToAdd.addAll(allValidNestedFiles);
         }
 
