cf326281482924451a038ac9e0b8926a66d13924
Matt Kaniaris <mkaniaris@gmail.com>
2014-06-11 13:30:31 -0700

    cleaned up select folder dialog

--- src/floobits/utilities/SelectFolder.java
+++ src/floobits/utilities/SelectFolder.java
@@ -7,7 +7,6 @@ import com.intellij.openapi.vfs.VirtualFile;
 import floobits.common.Constants;
 import floobits.common.RunLater;
 import floobits.common.Utils;
-import floobits.dialogs.DialogBuilder;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
@@ -20,7 +19,7 @@ public class SelectFolder {
         FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false);
         descriptor.setTitle("Select Folder For Workspace");
         descriptor.setDescription("NOTE: Floobits will NOT make a new, root directory inside the folder you choose. If you have cloned the project already, select that folder.");
-        String path = FilenameUtils.concat(Constants.baseDir, owner);
+        String path = FilenameUtils.concat(Constants.shareDir, owner);
         path = FilenameUtils.concat(path, workspace);
         File file = new File(path);
         try {
@@ -38,15 +37,6 @@ public class SelectFolder {
             return;
         }
         final String selectedPath = vFiles[0].getPath();
-        String title = String.format("Confirm path for %s", workspace);
-        String body = String.format("Save the workspace files for \"%s\" in \"%s\"?", workspace, path);
-        DialogBuilder.build(title, body, new RunLater<Boolean>() {
-            public void run(Boolean join) {
-                if (!join) {
-                    return;
-                }
-                runLater.run(selectedPath);
-            }
-        });
+        runLater.run(selectedPath);
     }
 }
