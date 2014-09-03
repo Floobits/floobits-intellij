ed7b5b38ef452b0b3d28a8eb13fd310c2621914c
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-02 18:13:28 -0700

    Fix showing wrong presentation when using command.

--- src/floobits/actions/FollowMode.java
+++ src/floobits/actions/FollowMode.java
@@ -1,11 +1,34 @@
 package floobits.actions;
 
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import floobits.FloobitsPlugin;
 import floobits.common.EditorEventHandler;
+import floobits.common.interfaces.IContext;
+import floobits.common.protocol.handlers.FlooHandler;
+import floobits.utilities.Flog;
+import org.jdesktop.swingx.action.ActionManager;
 
 public class FollowMode extends IsJoinedAction {
     public void actionPerformed(AnActionEvent e, EditorEventHandler editorEventHandler) {
-        boolean mode = editorEventHandler.follow();
+        editorEventHandler.follow();
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        FloobitsPlugin floobitsPlugin;
+        FlooHandler flooHandler;
+        floobitsPlugin = FloobitsPlugin.getInstance(e.getProject());
+        if (floobitsPlugin == null) {
+            Flog.log("No floobits plugin, aborting update.");
+            return;
+        }
+        flooHandler = floobitsPlugin.context.getFlooHandler();
+        if (flooHandler == null) {
+            return;
+        }
+        boolean mode = flooHandler.editorEventHandler.state.stalking;
         e.getPresentation().setText(String.format("%s follow mode", mode ? "Disable" : "Enable"));
     }
 }
