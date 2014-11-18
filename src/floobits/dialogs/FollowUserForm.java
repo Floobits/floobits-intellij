14dda6e9938b9986740f5607e7d2884c9ef1b5e2
Bjorn Tipling <bjorn@ambientchill.com>
2014-11-18 11:40:49 -0800

    Form for picking a user from a list.
    Issue #186

---
+++ src/floobits/dialogs/FollowUserForm.java
@@ -0,0 +1,11 @@
+package floobits.dialogs;
+
+import javax.swing.*;
+
+/**
+ * Created by bjorn on 11/18/14.
+ */
+public class FollowUserForm {
+    private JButton doneButton;
+    private JList userList;
+}
