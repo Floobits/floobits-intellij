dfb38cb00f0614cade6131a614649a7d62458de9
Bjorn Tipling <bjorn@ambientchill.com>
2015-03-06 13:52:06 -0800

    Stub out new chat layout. #199

---
+++ src/floobits/windows/ChatUserForm.java
@@ -0,0 +1,12 @@
+package floobits.windows;
+
+import javax.swing.*;
+
+/**
+ * Created by bjorn on 3/6/15.
+ */
+public class ChatUserForm {
+    private JList clientList;
+    private JLabel usernameLabel;
+    private JPanel gravatarContainer;
+}
