a7af6ea87bd9fad4da5e0e0dc05f006bf4c99597
Bjorn Tipling <bjorn@ambientchill.com>
2015-03-23 17:58:43 -0700

    Make white be white.

--- src/floobits/impl/DocImpl.java
+++ src/floobits/impl/DocImpl.java
@@ -102,7 +102,7 @@ public class DocImpl extends IDoc {
         attributes.setEffectColor(color);
         attributes.setEffectType(EffectType.SEARCH_MATCH);
         attributes.setBackgroundColor(color);
-        attributes.setForegroundColor(JBColor.WHITE);
+        attributes.setForegroundColor(Color.WHITE);
         int textLength = highlight.textLength;
         int userID = highlight.userid;
         LinkedList<RangeHighlighter> appliedHighlighters = getHighlightsForUser(highlight.path, userID);
