bb410fc2b06e386d27fd620754bafc1cfd6510ab
Bjorn Tipling <bjorn@ambientchill.com>
2015-04-08 18:50:29 -0700

    Add missing new file. #218

---
+++ src/floobits/actions/IsBaseProjectPath.java
@@ -0,0 +1,27 @@
+package floobits.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import floobits.utilities.Flog;
+
+public abstract class IsBaseProjectPath extends CanFloobits {
+
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        Project project = e.getProject();
+        if (project == null) {
+            return;
+        }
+        VirtualFile file = CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+        if (file == null) {
+            return;
+        }
+        String place = e.getPlace();
+        e.getPresentation().setEnabled(place.equals("MainMenu") || file.getPath().equals(project.getBasePath()));
+        e.getPresentation().setVisible(place.equals("MainMenu") || file.getPath().equals(project.getBasePath()));
+        Flog.warn("file is %s at %s", file.getPath(), place);
+    }
+}
