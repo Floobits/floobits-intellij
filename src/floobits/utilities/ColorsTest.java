2c2c0c23eac30597e785a5c0fb733756fec23faf
Bjorn Tipling <bjorn@ambientchill.com>
2015-04-29 16:17:46 -0700

    Added a basic test for Colors.

---
+++ src/floobits/utilities/ColorsTest.java
@@ -0,0 +1,36 @@
+package floobits.utilities;
+
+import com.intellij.ui.JBColor;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class ColorsTest {
+
+    @Test
+    public void testGetFGColor(){
+        JBColor expected = new JBColor(new Color(255, 255, 255, 255), new Color(255, 255, 255, 255));
+        JBColor received = Colors.getFGColor();
+        assertEquals("FG color should be black.", expected.getRGB(), received.getRGB());
+    }
+
+    @Test
+    public void testGetHex() {
+        JBColor c = new JBColor(new Color(255, 0, 0), new Color(255, 0, 0));
+        assertEquals("Should have gotten red hex value", "#FF0000", Colors.getHex(c));
+    }
+
+    @Test
+    public void testToHex() {
+        assertEquals("Should have gotten red hex value", "#FF0000", Colors.toHex(255, 0, 0));
+    }
+
+    @Test
+    public void testGetColorForUser() {
+        assertNotNull("testing get color for username", Colors.getColorForUser("bjorn"));
+    }
+}
