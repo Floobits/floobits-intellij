b03da98ad64bc7338dda9eadfb5cdd50d37afb0d
Geoff Greer <geoff@greer.fm>
2015-11-05 16:02:46 -0800

    Use translucent colors. Match with colors in Atom and web editor.

--- src/floobits/utilities/Colors.java
+++ src/floobits/utilities/Colors.java
@@ -7,28 +7,28 @@ import java.awt.*;
 
 public class Colors {
     static protected JBColor[] colors = new JBColor[] {
-        new JBColor(new Color(88,94,73),new Color(88,94,73)),
-        new JBColor(new Color(0,0,0),new Color(0,0,0)),
-        new JBColor(new Color(0,0,255),new Color(0,0,255)),
-        new JBColor(new Color(0,0,139),new Color(0,0,139)),
-        new JBColor(new Color(186,38,64),new Color(186,38,64)),
-        new JBColor(new Color(152,144,155),new Color(152,144,155)),
-        new JBColor(new Color(0,128,0),new Color(0,128,0)),
-        new JBColor(new Color(133,135,26),new Color(133,135,26)),
-        new JBColor(new Color(75,0,130),new Color(75,0,130)),
-        new JBColor(new Color(109,34,67),new Color(109,34,67)),
-        new JBColor(new Color(25,25,112),new Color(25,25,112)),
-        new JBColor(new Color(128,0,0),new Color(128,0,0)),
-        new JBColor(new Color(247,135,26),new Color(247,135,26)),
-        new JBColor(new Color(255,69,0),new Color(255,69,0)),
-        new JBColor(new Color(128,0,128),new Color(128,0,128 )),
-        new JBColor(new Color(255,0,0), new Color(255,0,0)),
-        new JBColor(new Color(0,128,128),new Color(0,128,128)),
-        new JBColor(new Color(252,219,116),new Color(252,219, 116)),
+        new JBColor(new Color(0, 255, 0, 77), new Color(0, 255, 0, 77)),
+        new JBColor(new Color(0, 0, 0, 77), new Color(0, 0, 0, 77)),
+        new JBColor(new Color(0, 0, 255, 77), new Color(0, 0, 255, 77)),
+        new JBColor(new Color(0, 0, 139, 77), new Color(0, 0, 139, 77)),
+        new JBColor(new Color(193, 84, 193, 77), new Color(193, 84, 193, 77)),
+        new JBColor(new Color(128, 128, 128, 77), new Color(128, 128, 128, 77)),
+        new JBColor(new Color(0, 128, 0, 77), new Color(0, 128, 0, 77)),
+        new JBColor(new Color(173, 255, 47, 77), new Color(173, 255, 47, 77)),
+        new JBColor(new Color(75, 0, 130, 77), new Color(75, 0, 130, 77)),
+        new JBColor(new Color(255, 0, 255, 77), new Color(255, 0, 255, 77)),
+        new JBColor(new Color(25, 25, 112, 77), new Color(25, 25, 112, 77)),
+        new JBColor(new Color(128, 0, 0, 77), new Color(128, 0, 0, 77)),
+        new JBColor(new Color(255, 165, 0, 77), new Color(255, 165, 0, 77)),
+        new JBColor(new Color(255, 69, 0, 77), new Color(255, 69, 0, 77)),
+        new JBColor(new Color(128, 0, 128, 77), new Color(128, 0, 128, 77)),
+        new JBColor(new Color(255, 0, 0, 77), new Color(255, 0, 0, 77)),
+        new JBColor(new Color(0, 128, 128, 77), new Color(0, 128, 128, 77)),
+        new JBColor(new Color(255, 255, 0, 77), new Color(255, 255, 0, 77))
     };
 
     public static JBColor getFGColor() {
-        return new JBColor(new Color(255,255,255,255),new Color(255,255,255,255));
+        return new JBColor(new Color(255, 255, 255, 255),new Color(255, 255, 255, 255));
     }
 
     // http://sny.no/2011/11/java-hex
